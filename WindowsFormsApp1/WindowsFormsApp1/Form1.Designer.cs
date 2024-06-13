55e75ef6405cba2dd6f72a3470f2c1b65fbe6122
이대은(Dae-eun Lee) <55821132+2daeeun@users.noreply.github.com>
2024-06-13 23:50:43 +0900

    [임시] GUI 프로그램

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(67, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 162);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "작은 상자";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(311, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 162);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "중간 상자";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button3.Location = new System.Drawing.Point(559, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 162);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "큰 상자";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Items.AddRange(new object[] {
+            "책상 시계",
+            "필통",
+            "컵",
+            "마우스",
+            "손목시계",
+            "짝퉁핸드폰",
+            "물병",
+            "물티슈",
+            "충전기",
+            "핸드크림",
+            "CD",
+            "동해물과",
+            "백두산이",
+            "마르고",
+            "닳도록",
+            "하느님이",
+            "보우하사",
+            "우리나라",
+            "만세",
+            "무궁화",
+            "삼천리",
+            "화려강산",
+            "대한사람",
+            "대한으로",
+            "길이보전",
+            "하세"});
+            this.listBox3.Location = new System.Drawing.Point(311, 217);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(150, 204);
+            this.listBox3.TabIndex = 9;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "나루토 피규어",
+            "아이패드 케이스",
+            "포토샵 책"});
+            this.listBox1.Location = new System.Drawing.Point(559, 217);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 204);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(734, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(299, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "컨베이어 벨트 IP : 182.78.46.12";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(734, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(212, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "서버 IP : 39.7.231.153";
+            // 
+            // listBox5
+            // 
+            this.listBox5.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 20;
+            this.listBox5.Items.AddRange(new object[] {
+            "연필",
+            "지우개",
+            "포스트지"});
+            this.listBox5.Location = new System.Drawing.Point(67, 217);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(150, 204);
+            this.listBox5.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 504);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        //private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox5;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ListBox listBox2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // listBox1 항목 추가
+            listBox1.Items.Add("원피스 피규어");
+            listBox1.Items.Add("수박");
+
+            // listBox1 이벤트 핸들러 추가
+            listBox1.SelectedIndexChanged += new EventHandler(listBox1_SelectedIndexChanged);
+
+            // 폼 클릭 이벤트 핸들러 추가
+            this.MouseClick += new MouseEventHandler(Form1_MouseClick);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedItem = listBox1.SelectedItem.ToString();
+
+                // 이미 생성된 listBox2가 있다면 제거
+                if (listBox2 != null)
+                {
+                    this.Controls.Remove(listBox2);
+                    listBox2.Dispose();
+                }
+
+                // 새로운 listBox2 생성
+                listBox2 = new ListBox();
+
+                if (selectedItem == "원피스 피규어")
+                {
+                    listBox2.Items.Add("상품 이름 : 원피스 피규어");
+                    listBox2.Items.Add("상품 구분 : 잡화");
+                    listBox2.Items.Add("발송자 : 홍길동");
+                    listBox2.Items.Add("수령자 : 이순신");
+                    listBox2.Items.Add("발송 지역 : 서울");
+                }
+                else if (selectedItem == "수박")
+                {
+                    listBox2.Items.Add("상품 이름 : 수박");
+                    listBox2.Items.Add("상품 구분 : 식품");
+                    listBox2.Items.Add("발송자 : 안중근");
+                    listBox2.Items.Add("수령자 : 유관순");
+                    listBox2.Items.Add("발송 지역 : 제주");
+                }
+
+                // listBox2 폰트 설정
+                listBox2.Font = new Font(listBox2.Font.FontFamily, 16);
+
+                // listBox2 위치와 크기 설정
+                listBox2.Location = new Point(listBox1.Right + 20, listBox1.Top);
+                listBox2.Size = new Size(300, 200);
+
+                // listBox2 클릭 이벤트 핸들러 추가
+                listBox2.MouseClick += new MouseEventHandler(listBox2_MouseClick);
+
+                // 폼에 새로운 listBox2 추가
+                this.Controls.Add(listBox2);
+            }
+        }
+
+        private void listBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            // listBox2의 클릭 이벤트를 처리하여 클릭된 영역을 확인합니다.
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // listBox2가 생성되어 있고, 클릭된 위치가 listBox2의 외부라면 listBox2를 제거합니다.
+            if (listBox2 != null && !listBox2.Bounds.Contains(e.Location))
+            {
+                this.Controls.Remove(listBox2);
+                listBox2.Dispose();
+                listBox2 = null;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
